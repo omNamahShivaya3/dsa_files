e36fd2df92d54ceb0ffcc1a51245cb94bfd46a6d
omNamahShivaya3 <121163296+omNamahShivaya3@users.noreply.github.com>
2024-02-05 08:48:43 +0000

    14:25 05-02-2024

--- lec-34/revStr.cpp
+++ lec-34/revStr.cpp
@@ -1,13 +1,23 @@
 #include<bits/stdc++.h>
-using namsespace std;
+using namespace std;
 
 void rev (string& s, int i, int j) {
     // base case
-    if (j>i) return;
+    if (i>j) return;
 
-    swap (s[i], s[j])
+    swap (s[i], s[j]);
+    i++;
+    j--;
+
+    rev (s, i, j);
+
 }
 
 int main () {
-
+    string name = "Vishal Kumar Pankaj";
+    
+    cout << "Before reversing: "<< name << endl;
+    rev (name, 0, name.length() - 1);
+    cout << "After reversing: "<< name << endl;
+    return 0;
 }
